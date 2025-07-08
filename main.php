3cd4d9dac67ed1a705059a413e60a3d35d18a9e5
rishisakariya <rishi.sakriya@gmail.com>
2025-07-08 18:22:41 +0530

    Update main.php

--- main.php
+++ main.php
@@ -6,12 +6,11 @@ $conn = new mysqli("localhost", "root", "", "user");
 if ($conn->connect_error) {
       die("Connection failed");
 }
-
-$userprofile = $_SESSION['username'];
-
-$query = "select * from register where Username='$userprofile'";
-$data = mysqli_query($conn, $query);
-$result = mysqli_fetch_assoc($data);
+// This data is for a session variable store
+//$userprofile = $_SESSION['username'];
+//$query = "select * from register where Username='$userprofile'";
+//$data = mysqli_query($conn, $query);
+//$result = mysqli_fetch_assoc($data);
 ?>
 
 
@@ -24,16 +23,17 @@ $result = mysqli_fetch_assoc($data);
       <title>Ric</title>
 </head>
 
-<body id="content" style="background-color:grey" ;>
-      <h6>WElCOME..! <?php echo "$userprofile" ?></h6>
+<body id="content" style="background-color:grey;"><!--content id is used for search content on site-->
 
-      <a href="logout.php">Logout</a>
+      <?php //echo "$userprofile" ?>
+
+      <!--<a href="logout.php">Logout</a>-->
 
       <!--navbar start-->
       <div>
-            <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <nav class="navbar navbar-expand-lg"><!--bg-body-tertiary-->
                   <div class="container-fluid">
-                        <a class="navbar-brand" href="#">RISHI SAKARIYA</a>
+                        <a class="navbar-brand" href="main.php">RISHI SAKARIYA</a>
                         <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                               data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                               aria-expanded="false" aria-label="Toggle navigation">
@@ -45,9 +45,12 @@ $result = mysqli_fetch_assoc($data);
                                           <a class="nav-link active" aria-current="page" href="main.php">Home</a>
                                     </li>
                                     <li class="nav-item">
-                                          <a class="nav-link" href="#">Link</a>
+                                          <a class="nav-link" href="about.php">AboutUs</a>
                                     </li>
-                                    <li class="nav-item dropdown">
+                                    <li class="nav-item">
+                                          <a class="nav-link" href="Contact.php">ContactUs</a>
+                                    </li>
+                                    <!--<li class="nav-item dropdown">
                                           <a class="nav-link dropdown-toggle" href="#" role="button"
                                                 data-bs-toggle="dropdown" aria-expanded="false">
                                                 Dropdown
@@ -60,19 +63,20 @@ $result = mysqli_fetch_assoc($data);
                                                 </li>
                                                 <li><a class="dropdown-item" href="#">DropDown3</a></li>
                                           </ul>
-                                    </li>
+                                    </li>-
                                     <li class="nav-item">
                                           <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-                                    </li>
+                                    </li>-->
                               </ul>
                               <form class="d-flex" role="search" id="searchForm">
                                     <input class="form-control me-2" type="search" id="searchInput" placeholder="Search"
                                           aria-label="Search" />
-                                    <button class="btn btn-outline-success" type="submit">Search</button>
+                                    <button class="btn btn-outline-success" type="submit"
+                                          style="background-color: black; color:white">Search</button>
                               </form>
 
-                              <button type="button" class="btn btn-primary" style="margin-left: 20px;"
-                                    onclick="window.location.href='Login.php'">Login</button>
+                              <button type="button" class="btn btn-primary" ;"
+                                    onclick="window.location.href='Login.php'">Logout</button>
 
 
                         </div>
@@ -82,17 +86,17 @@ $result = mysqli_fetch_assoc($data);
       <!--navbar end-->
 
       <!--slider start-->
-      <div id="carouselExample" class="carousel slide">
+      <div id="carouselExample" class="carousel slide" style="padding:10px 0px;">
             <div class="carousel-inner">
                   <div class="carousel-item active">
                         <!--<img src="1.png" class="d-block w-100" alt="pic1">-->
-                        <img src="4.jpg" class="d-block w-100" alt="pic1" style="height: 600px; object-fit: cover;">
+                        <img src="2.jpg" class="d-block w-100" alt="pic1" style="height: 600px; object-fit: cover;">
                   </div>
                   <div class="carousel-item">
                         <img src="2.jpg" class="d-block w-100" alt="pic2" style="height: 600px; object-fit: cover;">
                   </div>
                   <div class="carousel-item">
-                        <img src="3.jpg" class="d-block w-100" alt="pic3" style="height: 600px; object-fit: cover;">
+                        <img src="2.jpg" class="d-block w-100" alt="pic3" style="height: 600px; object-fit: cover;">
                   </div>
             </div>
             <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
@@ -106,6 +110,40 @@ $result = mysqli_fetch_assoc($data);
       </div>
 
       <!--slider end-->
+
+      <!--cards start-->
+      <div class="card mb-3" style="max-width: 100%; border-color:black;">
+            <div class="row g-0">
+                  <div class="col-md-4">
+                        <img src="4.jpg" class="img-fluid rounded-start" alt="...">
+                  </div>
+                  <div class="col-md-8" style="background-color: rgb(47, 44, 44); color: white;">
+                        <div class="card-body">
+                              <h5 class="card-title">Card title</h5>
+                              <p class="card-text">This is a wider card with supporting text below as a natural lead-in
+                                    to additional content. This content is a little bit longer.</p>
+                              </p>
+                        </div>
+                  </div>
+            </div>
+      </div>
+
+      <div class="card mb-3" style="max-width: 100%; border-color:black;">
+            <div class="row g-0">
+                  <div class="col-md-4">
+                        <img src="2.jpg" class="img-fluid rounded-start" alt="...">
+                  </div>
+                  <div class="col-md-8" style="background-color: rgb(47, 44, 44); color:white;">
+                        <div class="card-body">
+                              <h5 class="card-title">Card title</h5>
+                              <p class="card-text">This is a wider card with supporting text below as a natural lead-in
+                                    to additional content. This content is a little bit longer.</p>
+                              </p>
+                        </div>
+                  </div>
+            </div>
+      </div>
+      <!--cards end-->
       <!--<div>
       <div class="card" style="width: 18rem;">
   <img src="1.png " class="card-img-top" alt="picture">
@@ -135,10 +173,10 @@ $result = mysqli_fetch_assoc($data);
 </div>
 
 </div>-->
-
+      <!--Mini Card For Products Start-->
       <div class="d-flex justify-content-center gap-3 flex-wrap" style="padding:20px;">
 
-            <div class="card" style="width: 18rem; color: white; background-color:black; margin-right: 30px;">
+            <div class="card" style="width: 18rem; color: white; background-color:black;">
                   <img src="1.png" class="card-img-top" alt="picture">
                   <div class="card-body">
                         <h5 class="card-title">Card title</h5>
@@ -148,7 +186,7 @@ $result = mysqli_fetch_assoc($data);
                   </div>
             </div>
 
-            <div class="card" style="width: 18rem; color: white; background-color:black; margin-right: 30px;">
+            <div class="card" style="width: 18rem; color: white; background-color:black;">
                   <img src="1.png" class="card-img-top" alt="picture">
                   <div class="card-body">
                         <h5 class="card-title">Card title</h5>
@@ -158,7 +196,7 @@ $result = mysqli_fetch_assoc($data);
                   </div>
             </div>
 
-            <div class="card" style="width: 18rem; color: white; background-color:black; margin-right: 30px;">
+            <div class="card" style="width: 18rem; color: white; background-color:black;">
                   <img src="1.png" class="card-img-top" alt="picture">
                   <div class="card-body">
                         <h5 class="card-title">Card title</h5>
@@ -168,7 +206,7 @@ $result = mysqli_fetch_assoc($data);
                   </div>
             </div>
 
-            <div class="card" style="width: 18rem; color: white; background-color:black; margin-right: 30px;">
+            <div class="card" style="width: 18rem; color: white; background-color:black;">
                   <img src="1.png" class="card-img-top" alt="picture">
                   <div class="card-body">
                         <h5 class="card-title">Card title</h5>
@@ -178,7 +216,7 @@ $result = mysqli_fetch_assoc($data);
                   </div>
             </div>
       </div>
-
+      <!--Mini Card For Products Start-->
 </body>
 
 </html>
@@ -191,7 +229,7 @@ $result = mysqli_fetch_assoc($data);
       crossorigin="anonymous"></script>
 
 
-<!-- scricpt-->
+<!-- script-->
 
 
 <!--<div id="content">
@@ -233,3 +271,13 @@ $result = mysqli_fetch_assoc($data);
             }
       });
 </script>
+
+<style>
+      nav {
+            background-color: grey;
+      }
+
+      li {
+            background-color: auto;
+      }
+</style>
