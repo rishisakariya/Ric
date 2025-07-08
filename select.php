4fe4a4e23509c40aefea07f6ccc5e7c0af901480
rishisakariya <rishi.sakriya@gmail.com>
2025-07-08 18:23:29 +0530

    Update select.php

--- select.php
+++ select.php
@@ -1,30 +1,51 @@
-<?php
+<html>
 
-$conn = new mysqli("localhost", "root", "", "user");
+<head>
+    <link rel="stylesheet" href="style.css">
+    </link>
+</head>
+
+<body>
+    <?php
+
+    $conn = new mysqli("localhost", "root", "", "user");
 
 
-if ($conn->connect_error) {
-    die("Connection failed");
-}
-
-$sql = "select * from register";
-$result = $conn->query($sql);
-if ($result->num_rows > 0) {
-    echo "<table border=3 align='center'>";
-    echo "<tr>";
-    echo "<th>Username</th>";
-    echo "<th>Password</th>";
-    echo "</tr>";
-    // fetch associative array
-    while ($row = $result->fetch_assoc()) {
-        echo "<tr>";
-        echo "<td>" . $row['Username'] . "</td>";
-        echo "<td>" . $row['Password'] . "</td>";
-        echo "</tr>";
+    if ($conn->connect_error) {
+        die("Connection failed");
     }
-    echo "</table>";
-} else {
-    echo "No user found.";
-}
-$conn->close();
-?>
+
+    $sql = "select * from register";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        echo "<table border=3 align='center'>";
+        echo "<tr>";
+        echo "<th>Username</th>";
+        echo "<th>FirstName</th>";
+        echo "<th>LastName</th>";
+        echo "<th>Email</th>";
+        echo "<th>Gender</th>";
+        echo "<th>Password</th>";
+
+        echo "</tr>";
+        // fetch associative array
+        while ($row = $result->fetch_assoc()) {
+            echo "<tr>";
+            echo "<td>" . $row['Username'] . "</td>";
+            echo "<td>" . $row['fname'] . "</td>";
+            echo "<td>" . $row['lname'] . "</td>";
+            echo "<td>" . $row['email'] . "</td>";
+            echo "<td>" . $row['gender'] . "</td>";
+            echo "<td>" . $row['Password'] . "</td>";
+            echo "</tr>";
+        }
+        echo "</table>";
+    } else {
+        echo "No user found.";
+    }
+    $conn->close();
+    ?>
+
+</body>
+
+</html>
